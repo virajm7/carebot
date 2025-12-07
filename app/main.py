404ca6a3a3a1a3efa76949d5d479383e78681726
Viraj Mhadgut <mhadgutvirajwork@gmail.com>
2025-12-07 22:43:22 +0530

    first commit2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.router import router
 
 app = FastAPI(title="FAQBot with OpenRouter")
 
+# -----------------------
+# ENABLE CORS
+# -----------------------
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # you can restrict later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routes
 app.include_router(router)
